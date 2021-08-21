4f82355ae17841074572b3eebfd9c00855b8c015
victoriatrinita <victoriapardede@gmail.com>
2021-08-22 01:20:52 +0700

    feat: add DescKandangan and DescSarongan

---
+++ src/components/tour/DescKandangan.svelte
@@ -0,0 +1,33 @@
+<main>
+	<h1>About <span> Kandangan</span></h1>
+	<div>
+		<p>
+			Looking for a walk in a serene village to spend your leisure time? The exceptional Desa
+			Kandangan is the perfect place to hide from the reality of life.
+		</p>
+	</div>
+</main>
+
+<style>
+	h1 > span {
+		color: #0ca6c7;
+	}
+
+	h1 {
+		font-weight: 700;
+		text-align: center;
+		max-width: 84rem;
+		width: 100%;
+		padding: 0;
+		margin: 0em auto;
+		text-align: center;
+		position: relative;
+		font-size: clamp(1.25rem, 3vw, 2.5rem);
+	}
+	main {
+		padding: 2.5em;
+	}
+	div {
+		padding: 0 6em;
+	}
+</style>
---
+++ src/components/tour/DescSarongan.svelte
@@ -0,0 +1,33 @@
+<main>
+	<h1>About <span> Sarongan</span></h1>
+	<div>
+		<p>
+			Seeking a beautiful place in an isolated area to enjoy the beautiful scenery in Banyuwangi?
+			You can get all the splendid landscape in the magnificent Desa Sarongan.
+		</p>
+	</div>
+</main>
+
+<style>
+	h1 > span {
+		color: #0ca6c7;
+	}
+
+	h1 {
+		font-weight: 700;
+		text-align: center;
+		max-width: 84rem;
+		width: 100%;
+		padding: 0;
+		margin: 0em auto;
+		text-align: center;
+		position: relative;
+		font-size: clamp(1.25rem, 3vw, 2.5rem);
+	}
+	main {
+		padding: 2.5em;
+	}
+	div {
+		padding: 0 6em;
+	}
+</style>
