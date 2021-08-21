e9cb9780afcb4800215f01870a01d85a9bebebeb
victoriatrinita <victoriapardede@gmail.com>
2021-08-22 01:14:36 +0700

    feat: add AboutKandangan and AboutSarongan

---
+++ src/components/about/AboutKandangan.svelte
@@ -0,0 +1,47 @@
+<main>
+	<h1>The Exceptional <span> Desa Kandangan</span></h1>
+	<div>
+		<p>
+			Desa Kandangan is located in between Pesanggaran-Sarongan route at the foot of the Betiri
+			mountain. This beautiful area consists of forests, plantations, residential areas, and
+			individual farms. This land is productive and used for paddyfield, and farms for nuts,
+			horticultural crops, and fruits.
+			<br />
+			<br />
+			Desa Kandangan area is wide on the north side, and narrowed on the south side. In the west, its
+			territory is forest and plantations. While further to the east are residential areas and the shoreline.
+			Part of the conservation area of Meru Betiri National Park is located in this village. Desa Kandangan
+			has the shortest coastline compared to other villages in Pesanggaran sub-district.
+		</p>
+	</div>
+</main>
+
+<style>
+	h1 > span {
+		color: #0ca6c7;
+	}
+	h1 {
+		font-weight: 700;
+		text-align: center;
+		max-width: 84rem;
+		width: 100%;
+		padding: 0;
+		margin: 0em auto;
+		text-align: center;
+		position: relative;
+		font-size: clamp(1.25rem, 3vw, 2.5rem);
+	}
+
+    p{
+        text-align: justify;
+        line-height: 2em;
+    }
+
+	main {
+		padding: 2.5em;
+	}
+
+	div {
+		padding: 0 10em;
+	}
+</style>
---
+++ src/components/about/AboutSarongan.svelte
@@ -0,0 +1,48 @@
+<main>
+	<h1>The Magnificent <span> Desa Sarongan</span></h1>
+	<div>
+		<p>
+			Desa Sarongan’s territory is surrounded by mountains and hills, such as Mount Beteng (222
+			meters) and Mount Gendong (893) meters in the north. While in the south, this area is bordered
+			by The Indian Ocean with a beautiful coastline. Some of its areas such as the stunning
+			Rajegwesi Beach with its outstanding wild-life, Teluk Hijau, and Sukamade Beach are under the
+			auspices of Meru Betiri National Park.
+			<br />
+			<br />
+			Desa Sarongan has a quiet history back when the Indonesian government had not declared its independence
+			yet. You can find a military base of the Dutch and Japanese here. Now, this military base is used
+			by The Indonesian Navy at Rajegwesi beach, and the Indonesian Air Force post under The Air Force
+			Operations Command II Abdul Rachman Saleh Air Base.
+		</p>
+	</div>
+</main>
+
+<style>
+	h1 > span {
+		color: #0ca6c7;
+	}
+	h1 {
+		font-weight: 700;
+		text-align: center;
+		max-width: 84rem;
+		width: 100%;
+		padding: 0;
+		margin: 0em auto;
+		text-align: center;
+		position: relative;
+		font-size: clamp(1.25rem, 3vw, 2.5rem);
+	}
+
+	p {
+		text-align: justify;
+        line-height: 2em;
+	}
+
+	main {
+		padding: 2.5em;
+	}
+
+	div {
+		padding: 0 10em;
+	}
+</style>
