921db9b4674f24f7badc48de110f918bd782ec9f
victoriatrinita <victoriapardede@gmail.com>
2021-08-22 00:55:23 +0700

    feat: add nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,20 +1,61 @@
 <script>
 	export let segment;
+	import { stores } from '@sapper/app';
+	const { page } = stores();
+	$: check = (current) => ($page.path.startsWith(current) ? 'page' : undefined);
 </script>
 
+<nav>
+	<img src="assets/logo.png" alt="logo" href="." />
+
+	<ul>
+		<li>
+			<a aria-current={segment === undefined ? 'page' : undefined} href=".">Home</a>
+		</li>
+		<li>
+			<a aria-current={segment === 'about' ? 'page' : undefined} href="about">About</a>
+		</li>
+		<li>
+			<a aria-current={segment === 'gallery' ? 'page' : undefined} href="gallery">Gallery</a>
+		</li>
+		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
+		the blog data when we hover over the link or tap it on a touchscreen -->
+		<li>
+			<a rel="prefetch" aria-current={segment === 'article' ? 'page' : undefined} href="article"
+				>Article</a
+			>
+		</li>
+		<li>
+			<a aria-current={check('/tour/kandangan')} href="tour/kandangan">Tour Kandangan</a>
+		</li>
+		<li>
+			<a aria-current={check('/tour/sarongan')} href="tour/sarongan">Tour Sarongan</a>
+		</li>
+	</ul>
+</nav>
+
 <style>
+	a {
+		font-size: 1.25rem;
+	}
+
 	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
+		font-weight: bold;
+		padding: 1em 5em;
+		display: flex;
+		justify-content: space-between;
+		align-items: flex-end;
+	}
+
+	nav > img {
+		width: 17em;
+		cursor: pointer;
 	}
 
 	ul {
-		margin: 0;
-		padding: 0;
+		margin: auto 0;
 	}
 
-	/* clearfix */
 	ul::after {
 		content: '';
 		display: block;
@@ -34,27 +75,16 @@
 	[aria-current]::after {
 		position: absolute;
 		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(255,62,0);
+		width: calc(100% - 2em);
+		height: 0.1em;
+		background-color: #0ca6c7;
 		display: block;
 		bottom: -1px;
 	}
 
 	a {
 		text-decoration: none;
-		padding: 1em 0.5em;
+		padding: 0 1em;
 		display: block;
 	}
 </style>
-
-<nav>
-	<ul>
-		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
-		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
-	</ul>
-</nav>
