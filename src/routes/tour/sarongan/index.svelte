09b5cd3adf02847d6c3fc81617c2c1c49be84415
victoriatrinita <victoriapardede@gmail.com>
2021-08-22 01:22:44 +0700

    feat: add tour sarongan page

---
+++ src/routes/tour/sarongan/index.svelte
@@ -0,0 +1,13 @@
+<script>
+	import ArticlesGrid from '../../../components/articles/ArticlesGrid.svelte';
+	import Footer from '../../../components/Footer.svelte';
+	import CoverCarousel from '../../../components/home/CoverCarousel.svelte';
+	import PhotoCarousel from '../../../components/PhotoCarousel.svelte';
+	import DescSarongan from '../../../components/tour/DescSarongan.svelte';
+</script>
+
+<CoverCarousel title={'Discover Sarongan'} />
+<DescSarongan />
+<PhotoCarousel title={'Must See'} higlightedTitle={'Attractions'} />
+<ArticlesGrid />
+<Footer />
