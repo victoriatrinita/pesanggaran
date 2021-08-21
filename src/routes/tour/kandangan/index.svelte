8d298eb957f666ea08fd8368c9c10cdcbbb2cd6e
victoriatrinita <victoriapardede@gmail.com>
2021-08-22 01:22:33 +0700

    feat: add tour kandangan page

---
+++ src/routes/tour/kandangan/index.svelte
@@ -0,0 +1,13 @@
+<script>
+	import ArticlesGrid from '../../../components/articles/ArticlesGrid.svelte';
+	import Footer from '../../../components/Footer.svelte';
+	import CoverCarousel from '../../../components/home/CoverCarousel.svelte';
+	import PhotoCarousel from '../../../components/PhotoCarousel.svelte';
+	import DescKandangan from '../../../components/tour/DescKandangan.svelte';
+</script>
+
+<CoverCarousel title={'Discover Kandangan'} />
+<DescKandangan />
+<PhotoCarousel title={'Must See'} higlightedTitle={'Attractions'} />
+<ArticlesGrid />
+<Footer />
