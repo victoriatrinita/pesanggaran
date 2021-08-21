87832437471def2485bd97cbbbba6562a08acb02
victoriatrinita <victoriapardede@gmail.com>
2021-08-22 02:29:58 +0700

    feat: input article posts

---
+++ src/routes/article/_posts.js
@@ -0,0 +1,117 @@
+// Ordinarily, you'd generate this data from markdown files in your
+// repo, or fetch them from a database of some kind. But in order to
+// avoid unnecessary dependencies in the starter template, and in the
+// service of obviousness, we're just going to leave it here.
+
+// This file is called `_posts.js` rather than `posts.js`, because
+// we don't want to create an `/blog/posts` route — the leading
+// underscore tells Sapper not to do that.
+
+const posts = [
+	{
+		title: 'Liburan dengan Tetap di Rumah? Bisa, dan Tetap Seru dengan Berwisata Online?',
+		slug: 'liburan-dengan-tetap-di-rumah-bisa-dan-tetap-seru-dengan-berwisata-online',
+		date_published: '16 August, 2021',
+		html: `
+			<img src="https://i.imgur.com/EWjLPEH.png"></img>
+			<p>Indeed, at this time it is not an easy matter to stay at home and limit activities outside the home for some time to come. It is certain that some of the activities that you usually do, such as watching movies with friends and also traveling together, are temporarily stopped. But all this is done for the common good.</p>
+			<p>Don't worry, you can still enjoy the beauty of nature and the cultural diversity in Banyuwangi, even if you do it at home. You also don't have to worry about keeping a distance, because this trip can be done online.</p>
+			<p>Last February, the Banyuwangi Regency Government officially launched 102 events on the 2021 Banyuwangi Festival agenda. Unlike in previous years, the 2021 Banyuwangi Festival was held carrying the "Colorful Hybrid" concept which became a trend for traveling to Banyuwangi in the pandemic era. With a colorful hybrid concept, all tourist attraction agendas are carried out by combining offline and virtual events/attractions.</p>
+			<p>You can watch one of the agendas located in the Pesanggaran sub-district, namely, the South Beach Festival on August 10-17. This festival is a combination of 3 Petik Laut events on the south coast, namely Petik Laut Lampon, Petik Laut Rajegwesi, and Petik Laut Pancer (9 September).</p>
+			<p>Welcoming the celebration of the new year of the Javanese calendar, fishermen in the coastal area of ​​Lampon, Pesanggaran District held a Lampon Sea Picking. Petik Laut is an ancestral heritage which is referred to as community alms to the sea which for one year has been their place to earn sustenance. Every year the local fishermen carry out the traditional Petik Laut event. Similar to other traditional Petik Laut celebrations, they float the offerings into the middle of the sea by boat. The offerings made by fishermen are transported by mini boats. The offerings are the head of a cow and a number of agricultural and marine products.</p>
+			<p>Well, curious? Come on, enjoy the <a href='http://banyuwangitourism.com/festival/festival-pantai-selatan'>beauty of nature and the diversity of cultures in Banyuwangi online</a>!</p>
+			<p>It doesn't matter, right if you can travel when Indonesia's conditions are better? The important thing is that this online travel experience can keep you creative at home.</p>
+		`
+	},
+	{
+		title: 'Here are 5 beautiful beaches in Sarongan that you must visit!',
+		slug: '5-beautiful-beaches-in-sarongan-that-you-must-visit',
+		date_published: '14 August, 2021',
+		html: `
+			<p>Sarongan Village, Pesanggaran District presents a holiday atmosphere in the open which is still beautiful and quiet which offers a very tempting exotic beach. Curious about the true beauty that is stored in Sarongan? Come on, take a peek at 5 beautiful beaches that must be visited while in Sarongan below!</p>
+			<br>
+
+			<h2>1. Want Maritime Tourism? Rajegwesi Beach is the answer!</h2>
+			<img src="https://i.imgur.com/gXZ7eBC.jpeg"></img>
+			<p>Like other beaches in Indonesia, Rajegwesi also offers interesting marine tourism. Clear sea water with brown beach sand makes every tourist who visits it feel at home to linger on this beach. The waves are calm and the condition of the beach is quite clean, making Rajegwesi beach worth exploring.</p>
+			<p>Rajegwesi Beach is also known for the social activities of its people. The charm of this beach lies in its role, namely as a center of activity for fishermen. Tourists can meet fishermen who want to go to sea or dock. In addition, tourists can also see directly the fish auction activities that are also on this beach. Marine products from Rajegwesi are so abundant that they are able to become a source of seafood for several cities in Indonesia.</p>
+			<br>
+
+			<h2>2. The soothing sound of waves and rocks at Batu Beach</h2>
+			<img src="https://i.imgur.com/e02mrQ7.jpeg"></img>
+			<p>Located within Meru Betiri National Park, Batu Beach is a newly opened beach as one of the tourist destinations in Sarongan village. This beach has its own uniqueness for tourists. Batu Beach has a clean stretch of coral along its coast. This beach is still very rarely visited by tourists. Access to this beach is quite difficult because you have to pass through the Meru Betiri National Park route. Even so, all efforts to reach this beach will be paid off with a beautiful beach panorama and the soothing sound of waves and rocks.</p>
+			<br>
+			
+			<h2>3. Brown Sand and Calm Waves at Muara Mbaduk Beach</h2>
+			<img src="https://i.imgur.com/TWs1v7I.jpg"></img>
+			<p>Also known as Mumba Beach, Mbaduk beach is located at the mouth of the Buyuk River. Blessed with soothing brown sand and calm waves. Along the coast of this beach, you can see pandanus plants called pandansari by the locals. In addition, when you set foot on Mbaduk Beach, you will also be spoiled with beautiful views of green mountains.</p>
+			<br>
+
+			<h2>4. Quiet and Comfortable for relaxation at Permisan Beach</h2>
+			<img src="https://i.imgur.com/JkQA3t5.jpg"></img>
+			<p>Permisan Beach is a resting place for nature lovers who are trekking across Meru Betiri from Jember Bandealit to Sukamade Beach. On the west side of Permisan Beach, you can enjoy time with stunning waves and clean beach sand. While on the east side, where the waves of Permisan Beach are calm, it will definitely become your favorite place to explore and swim in the sea.</p>
+			<br>
+
+			<h2>5. Various Interesting Activities at Sukamade Beach</h2>
+			<img src="https://i.imgur.com/yIp7ZbV.jpg"></img>
+			<p>By walking east from Permisan Beach, you can reach Sukamade Beach in 5 hours. Located within Meru Betiri National Park, the distance between Sukamade Beach and Banyuwangi city center is about 100 km. You can take advantage of off-road jeeps to cross five tributaries. Visiting Sukamade Beach is no different from experiencing an unforgettable adventure. Sukamade Beach has been recognized as a conservation site since 1972, and is a favorite place for sea turtles to lay their eggs. WWF (World Wide Fund for Nature) states that the green turtle is an ordinary sea turtle. You can observe turtles that appear on Sukamade Beach to lay their eggs. They can also visit hatcheries and watch hatchlings release into the open sea, or even watch birds. Rivers and mangroves are perfect for bird watching.</p>
+			<p>The exotic beaches there are not only suitable for clearing your mind and having fun but also instagenic. In order to maintain the exoticism, keep it clean when you are at a tourist spot, OK!</p>
+		`
+	},
+	{
+		title: 'Can\'t Go on a vacation yet? Make a Travel Plan First',
+		slug: 'cant-go-on-a-vacation-yet-make-a-travel-plan-first',
+		date_published: '11 August, 2021',
+		html: `
+			<p>Indeed, conditions like today are not yet possible for us to spend time at tourist attractions, enjoy the coolness and beauty of nature and take vacations to tourist attractions outside the city. Even though there are limits to activities outside and spending a lot of time at home, you don’t need to be sad and worried first, because you can make travel plans so that the holidays will be more exciting.</p>
+			<ol>
+				<li>
+					<h2>Pick your destination</h2>
+					<p>It's okay if you have a passionate holiday desire right now. There must be a feeling of boredom when spending a lot of time at home. To make your vacation even more enjoyable, make sure to choose the best tourist destinations. You can do simple research, browse the internet, or ask friends about fun tourist spots to visit. Don't forget to think rationally in choosing the best tourist spots.</p>
+				</li>
+				<br>
+				<li>
+					<h2>Determine the Time and Length of Vacation</h2>
+					<p>If you already have a vacation spot to go to,The first thing you need to do when planning a vacation is to determine the time of departure, as well as how long you will spend. Don't take it lightly because it has an impact on destinations, booking travel tickets, to hotels or inns.</p>
+					<p>If you suddenly buy a plane ticket or other mode of transportation to go home and go, book a hotel because there is no preparation, of course it will affect the cost of your vacation. The duration of this vacation also determines the amount of luggage that will be brought during the holidays. Don't forget to bring along a mask, hand sanitizer and always maintain cleanliness while on vacation.</p>
+				</li>
+				<br>
+				<li>
+					<h2>Prepare the required budget</h2>
+					<p>After determining the date of departure, return, and length of vacation, then you have to start compiling the target budget needed. Detailing starting from the budget needs to buy plane tickets for round-trip (round-trip) or other modes of transportation, lodging costs, vehicle rental, entrance tickets to tourist attractions, food costs during the holidays, snacks, to the budget for buying souvenirs. But, don't forget to check the health standards and protocols for each type of transportation that you will use later.</p>
+				</li>
+				<br>
+				<li>
+					<h2>Preparing Accommodation</h2>
+					<p>When planning a vacation, remember to prepare accommodations. You cannot only focus on the vehicles used to get to tourist attractions, but also need to pay attention to hotels or places to stay. Book a hotel first before leaving for vacation. You can book in advance to avoid the possibility of the desired hotel being full and experiencing price increases.</p>
+				</li>
+				<br>
+				<li>
+					<h2>Make Itinerary</h2>
+					<p>There are several things that you need to consider when making an itinerary, such as the distance to tourist sites, the transportation to be used, opening and closing times of tourist attractions and don't forget to measure your stamina. </p>
+					<p>By utilizing several vacation support applications, looking for information from the web, blogs or vlogs related to the vacation spots that you are going to. Especially if you make plans with your vacation partner, it will be really fun.</p>
+					<p>Even though we still can't travel because of the pandemic, the travel plans that you have made can be very useful during the holidays in the future. Traveling must be memorable with careful planning. However, during the holidays, keep your health and cleanliness so that traveling remains comfortable and safe.</p>
+				</li>
+			</ol>
+		`
+	},
+	{
+		title: 'Teluk Hijau (Green Bay) : Beautiful Secluded Hidden Beach',
+		slug: 'teluk-hijau-green-bay-beautiful-secluded-hidden-beach',
+		date_published: '9 August, 2021',
+		html: `
+			<img src="https://i.imgur.com/sH3HYHv.jpg"></img>
+			<p>Located about 90 km on the south of the city of Banyuwangi, Teluk Hijau sits on the area of Sukamade Beach, Meru Betiri National Park Pesanggaran District, precisely in Sarongan village. It gets the name from the picture perfect landscape made of clear green surface highlighted with white sand shore and 8-meter high fresh waterfall. Usually this bay is visited by tourists who also visit Sukamade beach as it is located halfway. </p>
+			<p>Usually locals reach this spot on foot as it is located just approximately 30 minutes walk, and sometimes by motorcycle. The road from Rajegwesi to Teluk Hijau is considered to be very bumpy. And for visitors who stop by on the way to Sukamade usually ride on 4 WD.</p>
+			<p>Just stop at Teluk Damai (Peaceful Bay), that is where you can see nice blue green sea water on the southern ocean. It looks very beautiful, refreshing and peaceful as its name suggests. Visitors can take nice shots from the side of the road. </p>
+			<p>To reach Teluk Hijau, visitors should trek by the shoreline through stones. It takes about 30 minutes from Teluk Damai, passing shoreline and coastal forest. Watch out for strong waves that come in sudden After reaching the dead end turn right for a few meters ahead, a mesmerizing secluded beach is in front of your eyes.</p>
+			<p>Teluk Hijau is also a home for the rare and famous Rafflesia Arnoldii or “corpse flower”, and also the Japanese Cave where the soldiers built a fortress in World War II. The combination of green trees and blue water is a beautiful blend that will allure every visitor. It is like visiting a hidden Eden on Earth.</p>
+		`
+	},
+];
+
+posts.forEach(post => {
+	post.html = post.html.replace(/^\t{3}/gm, '');
+});
+
+export default posts;
