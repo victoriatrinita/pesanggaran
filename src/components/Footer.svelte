c4cecf1a3d536fd0fe2038396f343e0ee0ff764a
victoriatrinita <victoriapardede@gmail.com>
2021-08-22 00:44:04 +0700

    feat: add footer component

---
+++ src/components/Footer.svelte
@@ -0,0 +1,75 @@
+<div class="footer-wrapper">
+	<footer>
+		<section class="description">
+			<div>
+				<p>Desa Sarongan & Kandangan, Pesanggaran, Banyuwangi, Jawa Timur</p>
+			</div>
+			<div class="social-media">
+				<a href="https://www.youtube.com/channel/UCBB4IReBB9pgxSa2XFJe7CQ/featured">
+					<div class="contact-icon">
+						<img src="assets/youtube.svg" alt="Youtube" />
+					</div>
+				</a>
+				<a href="https://www.instagram.com/pesan_pesanggaran/">
+					<div class="contact-icon">
+						<img src="assets/instagram.svg" alt="Instagram" />
+					</div>
+				</a>
+			</div>
+		</section>
+	</footer>
+	<footer>
+		<div class="line" style="width: 96.752%;%" />
+		<section class="copyright">
+			<p>Copyright © 2021. KKN-PPM UGM Pesanggaran. All Rights Reserved.</p>
+		</section>
+	</footer>
+</div>
+
+<style>
+	.footer-wrapper {
+		padding: 3.334% 11.528%;
+		background: #fafafa;
+	}
+
+	footer p {
+		font-size: clamp(0.75rem, 3vw, 0.875rem);
+		color: #4a4a4a;
+	}
+	section {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	section a {
+		font-size: clamp(0.75rem, 3vw, 1rem);
+		line-height: 1.5em;
+		color: #4a4a4a;
+	}
+
+	.line {
+		padding-bottom: 10px;
+		border-bottom: 1px solid #4a4a4a;
+		margin-bottom: 10px;
+		width: 100%;
+	}
+	.social-media {
+		display: flex;
+		gap: 1em;
+	}
+
+	.contact-icon {
+		background: linear-gradient(0deg, #ffffff, #ffffff), #ffffff;
+		filter: drop-shadow(0px 4px 19px rgba(146, 146, 146, 0.2));
+		width: 3em;
+		height: 3em;
+		border-radius: 50%;
+		align-items: center;
+		justify-content: center;
+		display: flex;
+	}
+	.contact-icon img {
+		color: #4f4f4f;
+		width: 1.3em;
+	}
+</style>
