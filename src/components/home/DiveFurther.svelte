f4c8d8d666db961385106c1132b8ccbccbde4298
victoriatrinita <victoriapardede@gmail.com>
2021-08-22 01:08:26 +0700

    feat: add DiveFurther

---
+++ src/components/home/DiveFurther.svelte
@@ -0,0 +1,26 @@
+<div>
+	<h1>Dive <span>Further</span></h1>
+	<iframe
+		src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d252526.30322315547!2d113.83847854229037!3d-8.526203833492701!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd404e0b954f915%3A0x2de5697d9e651237!2sPesanggaran%2C%20Kabupaten%20Banyuwangi%2C%20Jawa%20Timur!5e0!3m2!1sid!2sid!4v1629561045693!5m2!1sid!2sid"
+		width="100%"
+		height="450"
+		style="border:0;"
+		allowfullscreen=""
+		loading="lazy"
+		title="map"
+	/>
+</div>
+
+<style>
+	h1 > span {
+		color: #0ca6c7;
+	}
+	h1 {
+		font-size: clamp(1.25rem, 3vw, 2.5rem);
+		font-weight: 700;
+		text-align: center;
+	}
+	div {
+		padding: 2.5em;
+	}
+</style>
