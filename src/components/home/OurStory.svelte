07f5814b5c87bbb04b01903595b0fc6a5d05577a
victoriatrinita <victoriapardede@gmail.com>
2021-08-22 01:08:04 +0700

    feat: add OurStory

---
+++ src/components/home/OurStory.svelte
@@ -0,0 +1,47 @@
+<main>
+	<h1>Our <span> Story</span></h1>
+	<div>
+		<p>
+			Pesanggaran tourism website is an attempt made by Universitas Gadjah Mada student study
+			services to promote destinations and socio-cultural of Desa Kandangan and Desa Sarongan,
+			Pesanggaran, Banyuwangi.
+			<br />
+			<br />
+			Wonderful destinations you can find in Pesanggaran suffice for dazzling beaches and a beautiful
+			range of hills. It is quite a view with the incredible hospitality from the local people which
+			brings calmness for you who want to spend a vacation and achieve peace of mind.
+			<br />
+			<br />
+			The relaxing blue sea waves of the coastline beaches in Desa Sarongan comes with an outstanding
+			landscape - yet, the green scenery and wild-life of Meru Betiri National Park you can enjoy.
+		</p>
+	</div>
+</main>
+
+<style>
+	h1 > span {
+		color: #0ca6c7;
+	}
+	p {
+		line-height: 2em;
+	}
+
+	h1 {
+		font-weight: 700;
+		text-align: center;
+		max-width: 84rem;
+		width: 100%;
+		padding: 0;
+		margin: 0em auto;
+		text-align: center;
+		position: relative;
+		font-size: clamp(1.25rem, 3vw, 2.5rem);
+	}
+	main {
+		padding: 2.5em;
+	}
+	div {
+		padding: 0 10em;
+		text-align: justify;
+	}
+</style>
