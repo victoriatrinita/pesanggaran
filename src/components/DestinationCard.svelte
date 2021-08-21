71d902fcbc3c8d1b958a86c42604399bda951c44
victoriatrinita <victoriapardede@gmail.com>
2021-08-22 01:20:15 +0700

    feat: add DestinationCard

---
+++ src/components/DestinationCard.svelte
@@ -0,0 +1,38 @@
+<a href="articles">
+	<div data-aos="fade-up">
+		<div>
+			<img src="assets/slide1.jpg" alt="destination" />
+		</div>
+	</div>
+</a>
+
+<style>
+	a > div {
+		display: grid;
+		gap: 1em;
+		align-content: flex-start;
+		border-radius: 0.75em;
+		background-color: #ffffff;
+		width: 20em;
+		height: 100%;
+		cursor: pointer;
+		box-shadow: 0px 3px 17px rgba(0, 0, 0, 0.14);
+		transition: all 300ms ease-out;
+	}
+	img {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+		max-height: 12em;
+		top: 0;
+		left: 0;
+		text-align: center;
+	}
+
+	@media only screen and (max-width: 768px) {
+		a > div {
+			width: 100%;
+			max-width: unset;
+		}
+	}
+</style>
